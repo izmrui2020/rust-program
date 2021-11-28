723c71a5573f9fbdca8fb24c8148b9f4ee0f1c6f
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-11-28 19:18:40 +0900

    save

--- asynchronous_tokio/src/main.rs
+++ asynchronous_tokio/src/main.rs
@@ -1,26 +1,59 @@
+use bytes::Bytes;
+use mini_redis::Command::{self, Get, Set};
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
 
-async fn process(socket: TcpStream) {
+struct myMutex {
+    mutex: Mutex<i32>,
+}
+impl myMutex {
+    fn insert_data(&self) {
+        let mut lock = self.mutex.lock().unwrap();
+    }
+}
+
+type ShardedDb = Arc<Mutex<HashMap<String, Vec<u8>>>>;
+async fn process(socket: TcpStream, db: ShardedDb) {
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("got = {:?}", frame);
-        let response = Frame::Error("unimplemented".to_string());
+    // Use `read_frame` to receive a command from the connection.
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                println!("db is {:?}", db);
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                let mut db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+        // Write the response to the client
         connection.write_frame(&response).await.unwrap();
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Listening");
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
+        println!("Accepted");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
-
-    Ok(())
 }
