3dfed50bc99a73e4c2326f5ccefd055e3d1d3d92
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-12-05 18:57:54 +0900

    the book 8

--- tmp/src/main.rs
+++ tmp/src/main.rs
@@ -1,3 +1,25 @@
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    // 最大値は{}です
+    println!("The largest number is {}", largest);
 }
