f402df56c9974a4c43c81427c7a28b3d984b892d
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-12-05 19:19:31 +0900

    the book trait

--- tmp/src/main.rs
+++ tmp/src/main.rs
@@ -1,25 +1,48 @@
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
 
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
-    largest
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+//traitにあるメソッドを使った関数をプラスで作りたい場合。
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            // もちろん、ご存知かもしれませんがね、みなさん
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
 
-    let mut largest = number_list[0];
-
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    // 最大値は{}です
-    println!("The largest number is {}", largest);
+    notify(&tweet);
 }
