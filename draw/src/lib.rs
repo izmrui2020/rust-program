43a9c42f63632b6e40b44077733bbd1fd23485b0
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-12-09 21:06:27 +0900

    exercism

--- draw/src/lib.rs
+++ draw/src/lib.rs
@@ -1,32 +1,3 @@
-pub trait Draw {
-    fn draw(&self);
-}
-
-pub struct Screen {
-    pub components: Vec<Box<dyn Draw>>,
-}
-
-impl Screen {
-    pub fn run(&self) {
-        for component in self.components.iter() {
-            component.draw();
-        }
-    }
-}
-
-// pub struct Screen<T: Draw> {
-//     pub components: Vec<T>,
-// }
-
-// impl<T> Screen<T>
-//     where T: Draw {
-//     pub fn run(&self) {
-//         for component in self.components.iter() {
-//             component.draw();
-//         }
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
     #[test]
--- tmp/src/main.rs
+++ tmp/src/main.rs
@@ -1,18 +1,28 @@
-use std::fmt::Display;
-
-pub trait Ranking {
-    fn run();
-}
-
 pub trait Draw {
     fn draw(&self);
 }
 
-pub struct Screen {
-    pub components: Vec<Box<Draw>>,
+// pub struct Screen {
+//     pub components: Vec<Box<dyn Draw>>,
+// }
+
+// impl Screen {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
+
+///これは一つの方のみしか適応がされない
+pub struct Screen<T: Draw> {
+    pub components: Vec<Box<T>>,
 }
 
-impl Screen {
+impl<T> Screen<T>
+where
+    T: Draw,
+{
     pub fn run(&self) {
         for component in self.components.iter() {
             component.draw();
@@ -20,10 +30,52 @@ impl Screen {
     }
 }
 
-pub struct Amount<T: Ranking + Display> {
-    pub components: Vec<T>,
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
 }
 
-impl<T> Amount<T> where T: Ranking + Display {}
+impl Draw for Button {
+    fn draw(&self) {}
+}
 
-fn main() {}
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+        //セレクトボックスを実際に描画するコード
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    // はい
+                    String::from("Yes"),
+                    // 多分
+                    String::from("Maybe"),
+                    // いいえ
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                // 了解
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
