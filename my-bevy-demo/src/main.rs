d33d3524df65ec506fc64b3d464e1f817ca4da48
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-12-13 22:28:58 +0900

    file managemant

--- my-bevy-demo/src/main.rs
+++ my-bevy-demo/src/main.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+fn print_position_system(query: Query<&Transform>) {
+    for transform in query.iter() {
+        println!("position: {:?}", transform.translation);
+    }
+}
+
+struct Entity(u64);
+
+fn hello_world() {
+    println!("hello world!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::build().add_system(hello_world.system()).run();
 }
