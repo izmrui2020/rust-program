df0da440cdd6d7f7cf304cf7a1b4c538b5ece56b
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-12-24 23:49:09 +0900

    trati pre

--- learn-std/src/main.rs
+++ learn-std/src/main.rs
@@ -1,15 +1,69 @@
+use std::collections::HashMap;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+trait Hoge {
+    fn hoge();
+}
+
+#[derive(Default)]
+struct A {
+    hoge: String
+}
+impl Hoge for A {fn hoge(){}}
+
+#[derive(Default)]
+struct B {
+    hoge: String,
+}
+impl Hoge for B {fn hoge(){}}
+
+#[derive(Default)]
+struct C {
+    hoge: String,
+}
+impl Hoge for C {fn hoge(){}}
+
+
+#[derive(EnumIter)]
+enum StructTypes<T: Hoge + Default> {
+    A(T),
+    B(T),
+    C(T),
+}
+
+impl<T> StructTypes<T>
+where
+    T: Hoge + Default
+{
+    fn new(&self) {
+        match self {
+            A(_) => {self.A.hoge()},
+            B(_) => {self.hoge()},
+            C(_) => {self.hoge()},
+        }
+    }
+}
+
+struct EnumMaster<T: Hoge + Default> {
+    store: HashMap<String, StructTypes<T>>
+}
+
+impl<T> EnumMaster<T>
+where
+    T: Hoge + Default
+{
+    
+    fn hoge() {
+        for i in StructTypes::<T>::iter() {
+            
+        }
+    }
+}
+
+
+
+
+
 fn main() {
-    let num = vec![5];
-    let arg = vec![6];
-
-    println!("num: {:p}", &num);
-    println!("arg: {:p}", &arg);
-
-    let c2 = |arg| {
-        println!("num: {:p}", &num);
-        println!("arg: {:p}", arg);
-    };
-    c2(&arg);
-    println!("num: {:p}", &num);
-    println!("arg: {:p}", &arg);
+    println!("Hello, world!");
 }
