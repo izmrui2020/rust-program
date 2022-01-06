bbea527d9149ac383a78da6223e31a5a7c6e9625
rr-rr-re <rr.anonymous.2020@gmail.com>
2022-01-06 21:45:02 +0900

    anyhow

--- anyhow/src/main.rs
+++ anyhow/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
+use std::fmt::Display;
+use std::error::Error;
+use anyhow::{Result, Context, anyhow};
+
+#[derive(Debug)]
+enum MyError {
+    HogeError(u8),
+    FooError(String),
+}
+
+impl Display for MyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        use self::MyError::*;
+        match self {
+            HogeError(i) => write!(f, "HogeError: {}", i),
+            FooError(s) => write!(f, "FooError: {}", s),
+        }
+    }
+}
+
+impl Error for MyError {}
+    
+
+fn hoge(filename: &str) -> anyhow::Result<()> {
+    //let f = std::fs::File::open(filename).with_context(|| format!("faile to open filr: {}", filename))?;                 // std::io::Errorを返す
+    
+    std::fs::File::open(filename).map_err(|e| anyhow!(e))?;
+    
+    // ...
+    Err(MyError::FooError("foo".to_string()))?;    // MyErrorを返す
+    // ...
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    
+    // tokio::spawn(async move {
+    //     hoge("hogehoge.tx").await.context("faile at hoge()")?;
+    // });
+    
+    hoge("hogehoge.tx").await?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
