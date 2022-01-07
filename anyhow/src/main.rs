35f679197a362381a01d264237be5461edffbcad
rr-rr-re <rr.anonymous.2020@gmail.com>
2022-01-07 20:47:27 +0900

    anyhow

--- anyhow/src/main.rs
+++ anyhow/src/main.rs
@@ -1,47 +1,77 @@
-use std::fmt::Display;
-use std::error::Error;
-use anyhow::{Result, Context, anyhow};
+// use anyhow::*;
+// use tokio::fs::File;
 
-#[derive(Debug)]
-enum MyError {
-    HogeError(u8),
-    FooError(String),
-}
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     File::open("foo.txt").await?;
+//     Ok(())
 
-impl Display for MyError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        use self::MyError::*;
-        match self {
-            HogeError(i) => write!(f, "HogeError: {}", i),
-            FooError(s) => write!(f, "FooError: {}", s),
-        }
-    }
-}
+// }
 
-impl Error for MyError {}
+// use anyhow::*;
+// use tokio::fs::File;
+
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     File::open("foo.txt").await?;
+//     Ok(())
+
+// }
+
+
+// use std::fmt::Display;
+// use std::error::Error;
+// use anyhow::{Result, Context, anyhow};
+
+// #[derive(Debug)]
+// enum MyError {
+//     HogeError(u8),
+//     FooError(String),
+// }
+
+// impl Display for MyError {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         use self::MyError::*;
+//         match self {
+//             HogeError(i) => write!(f, "HogeError: {}", i),
+//             FooError(s) => write!(f, "FooError: {}", s),
+//         }
+//     }
+// }
+
+// impl Error for MyError {}
     
 
-fn hoge(filename: &str) -> anyhow::Result<()> {
-    //let f = std::fs::File::open(filename).with_context(|| format!("faile to open filr: {}", filename))?;                 // std::io::Errorを返す
+// fn hoge(filename: &str) -> anyhow::Result<()> {
+//     //let f = std::fs::File::open(filename).with_context(|| format!("faile to open filr: {}", filename))?;                 // std::io::Errorを返す
     
-    std::fs::File::open(filename).map_err(|e| anyhow!(e))?;
+//     std::fs::File::open(filename).map_err(|e| anyhow!(e))?;
     
-    // ...
-    Err(MyError::FooError("foo".to_string()))?;    // MyErrorを返す
-    // ...
-    Ok(())
-}
+//     // ...
+//     Err(MyError::FooError("foo".to_string()))?;    // MyErrorを返す
+//     // ...
+//     Ok(())
+// }
+
+use tokio::fs::File;
+//use tokio::sync::mpsc;
+use async_channel::*;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<(), Box<dyn Error + Send + Sync + 'static>> {
+async fn main() -> anyhow::Result<()> {
+
+    let (tx, rx) = bounded(32).await;
+
     
-    // tokio::spawn(async move {
-    //     hoge("hogehoge.tx").await.context("faile at hoge()")?;
-    // });
+    let handle = tokio::spawn( async move {
+        File::open("hogehoge").await
+    });
     
-    hoge("hogehoge.tx").await?;
+    //hoge("hogehoge.tx").await?;
 
     println!("Hello, world!");
+
+    handle.await??;
 
     Ok(())
 }
