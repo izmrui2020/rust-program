94aba3d43a7f5845610bb7afbb216d6884803089
rr-rr-re <rr.anonymous.2020@gmail.com>
2022-01-12 21:41:39 +0900

    fix tokio::spawn

--- store/learn-std/src/main.rs
+++ store/learn-std/src/main.rs
@@ -1,31 +1,56 @@
 use std::{collections::HashMap};
 use anyhow::{Result, anyhow};
 use tokio::sync::mpsc;
+use tokio::sync::mpsc::Sender;
+use std::sync::{Arc,Mutex};
 use async_trait::async_trait;
 
 #[async_trait]
 pub trait Interface {
-    fn create(&self);
-    fn target_method(&mut self) -> Result<()>;
+    fn create(&mut self);
+    async fn target_method(&'static mut self) -> Result<()>;
 }
 pub struct Data {
     nama: String,
     id: String,
 }
+#[async_trait]
 pub trait Fuga {
     fn hogehoge(&self);
     fn update(&mut self) -> Result<()>;
     fn down(&mut self) -> Result<()>;
 }
-pub struct Hoge {
-    store: HashMap<String, Box<dyn Fuga + Send + 'static>>,
+
+pub struct Hoge<T> 
+where
+    T: Fuga + Send + Default + 'static,
+{
+    store: HashMap<String, Box<T>>,
+}
+
+async fn target_process_async<T>(
+    target: &'static mut Box<T>,
+    tx: Sender<Result<()>>,
+) -> Result<()>
+where
+    T: Fuga + Send + Default + 'static,
+{
+    let res = target.update();
+    tx.send(res).await?;
+
+    let res = target.down();
+    tx.send(res).await?;
+    Ok(())
 }
 
 #[async_trait]
-impl Interface for Hoge {
-    fn create(&self) {
+impl<T> Interface for Hoge<T> 
+where
+    T: Fuga + Send + Default + 'static
+{
+    fn create(&mut self) {
         for i in (0..10) {
-            let data = Sub::default();
+            let data = T::default();
 
             self.store.insert(
                 i.to_string(),
@@ -33,28 +58,35 @@ impl Interface for Hoge {
             );
         }
     }
-    async fn target_method(&mut self) -> Result<()> {
+    async fn target_method(&'static mut self) -> Result<()> {
         
-        let (tx, rx) = mpsc::channel(32);
-        
-        self.store.iter_mut().for_each(|(_, target)| {
+        let (tx, mut rx) = mpsc::channel(32);
+        let mut handles = vec![];
+
+        self.store.iter_mut().for_each(|(key, target)| {
             let clone_tx1 = tx.clone();
-            let clone_tx2 = tx.clone();
+                let task1 = tokio::spawn(async move {
+                    let res = target.update();
+                    match clone_tx1.send(res).await {
+                        Err(e) => anyhow::bail!(e),
+                        _ => {}
+                    }
 
-            let task1 = tokio::spawn(async move {
-                let res = target.update();
-                clone_tx1.send(res).await;
-            });
+                    let res = target.down();
+                    match clone_tx1.send(res).await {
+                        Err(err) => anyhow::bail!(err),
+                        _ => {}
+                    }
+                    Ok(())
 
-            let task2 = tokio::spawn(async move {
-                let res = target.down();
-                clone_tx2.send(res).await;
+            
             });
+            handles.push(task1);
 
         });
 
         while let Some(v) = rx.recv().await {
-            println!("hgoe:{:?}", &v);   
+            println!("hgoe:{:?}", &v);
         };
 
         
@@ -67,6 +99,7 @@ pub struct Sub {
     hoge: String
 }
 
+#[async_trait]
 impl Fuga for Sub {
     fn hogehoge(&self) {
         
