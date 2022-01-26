8377cb0153d1bee9f1b43ced0f1373fb77925eef
rr-rr-re <rr.anonymous.2020@gmail.com>
2022-01-26 23:29:08 +0900

    merge sort

---
+++ merge_sort/src/main.rs
@@ -0,0 +1,33 @@
+//implment mergesort
+use bigdecimal::BigDecimal;
+
+struct Data {
+    id: String,
+    value: BigDecimal,
+    diff: BigDecimal,
+}
+
+impl Data {
+    fn new(
+        id: String,
+        value: BigDecimal,
+        diff: BigDecimal,
+    ) -> Self {
+        Self {
+            id,
+            value,
+            diff
+        }
+    }
+    
+}
+
+
+pub merge() -> Result<()> {
+    
+}
+
+fn main() {
+    
+    println!("Hello, world!");
+}
--- my_sort_algorithm/src/sort.rs
+++ my_sort_algorithm/src/sort.rs
@@ -2,6 +2,8 @@
 use rand::prelude::*;
 use rand::Rng;
 use bigdecimal::BigDecimal;
+use std::cmp::Ordering;
+use std::vec::Vec;
 use std::{collections::HashMap, hash::Hash};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -42,6 +44,74 @@ impl Data {
             diff: Diff::default()
         }
     }
+
+    
+
+    // fn my_sort_algorithm<F>(&mut self, mut compare: F)
+    // where
+    //     F: FnMut(BigDecimal, BigDecimal) -> Ordering,
+    // {
+    //     self.merge_sort(|a, b| compare(a, b) == Ordering::Less);
+    // }
+}
+
+fn my_merge_sort(mut arr: Vec<i32>, left: usize, right: usize) -> Vec<i32> {
+    if right - 1 > left {
+        let mid = left + (right - left) / 2;
+        arr = merge_sort(arr, left, mid);
+        arr = merge_sort(arr, mid, right);
+        arr = merge(arr, left, mid, right);
+    }
+    arr
+}
+
+pub fn merge(mut arr: Vec<i32>, left: usize, mid: usize, right: usize) -> Vec<i32> {
+    let n1 = mid - left;
+    let n2 = right - mid;
+    let mut L1 = arr.clone();
+    let mut R1 = arr.clone();
+    let L = &L1[left..mid];
+    let R = &R1[mid..right];
+    /* Merge the temp arrays back into arr[l..r]*/
+    let mut i = 0; // Initial index of first subarray
+    let mut j = 0; // Initial index of second subarray
+    let mut k = left; // Initial index of merged subarray
+    while i < n1 && j < n2 {
+        if L[i] < R[j] {
+            arr[k] = L[i];
+            i = i + 1;
+        } else {
+            arr[k] = R[j];
+            j = j + 1;
+        }
+        k = k + 1;
+    }
+    while i < n1 {
+        arr[k] = L[i];
+        i = i + 1;
+        k = k + 1;
+    }
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while j < n2 {
+        arr[k] = R[j];
+        j = j + 1;
+        k = k + 1;
+    }
+    arr
+}
+
+pub fn calc_random_walk_steps(r:i64) -> i64 {
+    let directions = [[-1,0],[1,0],[0,-1],[0,1]];
+    let mut position = [0i64,0];
+    let mut steps = 0;
+    while (position[0].abs() + position[1].abs()) < r {
+        let dir_index = rand::random::<usize>()%4;
+        position[0]+=directions[dir_index][0];
+        position[1]+=directions[dir_index][1];
+        steps+=1;
+    }    
+    steps
 }
 
 struct Mdata {
