3fbc33685a119fc9f012ac04b4c6515fa7878634
rr-rr-re <rr.anonymous.2020@gmail.com>
2022-01-28 22:53:40 +0900

    time manager

--- store/date-manage/src/main.rs
+++ store/date-manage/src/main.rs
@@ -1,3 +1,24 @@
+use core::time;
+
+//
+use anyhow::Result;
+use chrono::{Utc, Date, DateTime, Duration, NaiveDateTime, NaiveDate, NaiveTime, FixedOffset};
+use chrono::TimeZone;
+
+pub fn time_handler(prev: u32) -> NaiveDateTime {
+    // こんな感じで時間は出せる。
+    let now: DateTime<Utc> = Utc::now();
+    let j_now = now + Duration::hours(9);
+    let dt1 = j_now - Duration::days(prev.into());
+    
+    dt1.naive_utc()
+}
 fn main() {
-    println!("Hello, world!");
+
+    for i in 1..23 {
+        let prev = time_handler(i);
+        println!("Hello, world! {:?}", prev);
+    }
+    
+    
 }
