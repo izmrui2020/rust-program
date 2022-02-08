9cb7d3345f60b371f53ae7bf2d04da67ec790179
izmrui2020 <rui.iizumi.20@gmail.com>
2022-02-08 21:13:37 +0900

    tonic

--- search/rust-tonic2/client/src/main.rs
+++ search/rust-tonic2/client/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+//
+use communicate::conn_client::ConnClient;
+use communicate::EchoRequest;
+use serde::Deserialize;
+use tonic::Streaming;
+
+pub mod communicate {
+    tonic::include_proto!("communicate");
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Config {
+    host: String,
+    port: u16,
+}
+
+impl std::default::Default for Config {
+    fn default() -> Self {
+        Self {
+            host: "localhost".into(),
+            port: 20000,
+        }
+    }
+}
+impl Config {
+    fn load() -> Config {
+        let mut cfg = config::Config::default();
+        cfg.merge(config::Environment::with_prefix("client")).unwrap();
+        cfg.try_into().unwrap_or_else(|_| Default::default())
+    }
+    
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cfg = Config::load();
+
+    let mut client = ConnClient::connect(
+        format!("http://{}:{}", cfg.host, cfg.port)).await?;
+
+    let request = tonic::Request::new(EchoRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.echo(request).await?;
+
+    println!("{:?}", response);
+    Ok(())
 }
