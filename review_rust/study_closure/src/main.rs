45e8ec045277b95fdbea06c4916a01e69d396c3d
izmrui2020 <rui.iizumi.20@gmail.com>
2022-02-28 22:08:17 +0900

    closure

---
+++ review_rust/study_closure/src/main.rs
@@ -0,0 +1,102 @@
+
+//
+use std::thread;
+use std::time::Duration;
+
+// fn simulated_expensive_calculation(intensity: u32) -> u32 {
+//     println!("calculating slowly...");
+//     thread::sleep(Duration::from_secs(2));
+//     intensity
+// }
+
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly..");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+    
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result.value(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result.value(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn main() {
+    // let simulated_user_specified_value = 10;
+    // let simulated_random_number = 7;
+
+    // generate_workout(
+    //     simulated_user_specified_value,
+    //     simulated_random_number
+    // );
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
