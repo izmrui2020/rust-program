e46c0d4a1a5db124b3256036a844aaa6581884bb
izmrui2020 <rui.iizumi.20@gmail.com>
2022-03-04 22:07:10 +0900

    async executer

--- review_rust/async-bok/src/main.rs
+++ review_rust/async-bok/src/main.rs
@@ -1,3 +1,43 @@
+//
+use {
+    futures::{
+        future::{FutureExt, BoxFuture},
+        task::{ArcWake, waker_ref},
+    },
+    std::{
+        future::Future,
+        sync::{Arc, Mutex},
+        sync::mpsc::{sync_channel, SyncSender, Receiver},
+        task::{Context, Poll},
+        time::Duration,
+    },
+    
+};
+
+mod future;
+use future::TimerFuture;
+
+struct Executor {
+    ready_queue: Receiver<Arc<Task>>,
+}
+
+#[derive(Clone)]
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+struct Task {
+    future: Mutex<Option<BoxFuture<'static, ()>>>,
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+fn new_executor_and_spawner() -> (Executor, Spawner) {
+    const MAX_QUEUED_TASKS: usize = 10_000;
+    let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
+    (Executor { ready_queue }, Spawner { task_sender})
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    
 }
